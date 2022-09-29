009fb00f3f2d1f228f68ba2f57610a2928637fca
jsong73 <jessicajinsong@gmail.com>
2022-09-29 19:39:54 -0400

    starting js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// var yelpApiKey = "xMJSZcLtoDztdGXkQ-A8PVyuQFPA0GUMLBS44-TAYY0VOpXcR2uGZilymNwno34PmkS1gAJqE6ubegWAl0ztFLcriZz6A_7gWfM7YeXApX4rd7yOykUW4YuHpc4zY3Yx";
+// var yelpUrl = "https://api.yelp.com/v3/businesses/search";
+// var location = "";
+// var rating = "";
+// var price = "";
+
+var restaurant = document.getElementById("restaurant-search");
+var location = document.getElementById("location-search");
+var searchBtn = document.getElementById("search-icon");
+
+console.log("HI")
