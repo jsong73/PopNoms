1b351dadcbbf0c12bf9ca3a3a03d0d6a690e19e8
jsong73 <jessicajinsong@gmail.com>
2022-10-01 23:34:23 -0400

    pulling from main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 //must include bearer in front of api key
 var yelpApiKey = "Bearer xMJSZcLtoDztdGXkQ-A8PVyuQFPA0GUMLBS44-TAYY0VOpXcR2uGZilymNwno34PmkS1gAJqE6ubegWAl0ztFLcriZz6A_7gWfM7YeXApX4rd7yOykUW4YuHpc4zY3Yx";
-var yelpUrl = "https://api.yelp.com/v3/businesses/search";
+var yelpUrl = "https://api.yelp.com/v3/businesses/search?";
 var corsUrl = "https://stormy-cliffs-87695.herokuapp.com/"
 var rating = "";
 var price = "";
-var city = "";
+var city = "location"
 
 var restaurantInput = document.getElementById("restaurant-search");
 var locationInput = document.getElementById("location-search");
@@ -14,7 +14,8 @@ function getData(event){
     // event.preventDefault();
 
 //if you get CORS error include the cors URL infront of your API URL
-    var requestData = corsUrl + "https://api.yelp.com/v3/businesses/search?location=atlanta&categories=chinese&price=1;" 
+    var requestData = corsUrl + yelpUrl + city
+    // "https://api.yelp.com/v3/businesses/search?location=atlanta&categories=chinese&price=1;" 
 
 fetch(requestData, {
     headers:{
