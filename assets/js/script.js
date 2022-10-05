73717a41d0acc39354dfad097f0eb1eb1b31551d
jsong73 <jessicajinsong@gmail.com>
2022-10-05 01:53:20 -0400

    adding to my styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 var yelpApiKey = "Bearer xMJSZcLtoDztdGXkQ-A8PVyuQFPA0GUMLBS44-TAYY0VOpXcR2uGZilymNwno34PmkS1gAJqE6ubegWAl0ztFLcriZz6A_7gWfM7YeXApX4rd7yOykUW4YuHpc4zY3Yx";
 var yelpUrl = "https://api.yelp.com/v3/businesses/search?";
 var corsUrl = "https://stormy-cliffs-87695.herokuapp.com/";
+
 var ratings = "sort_by=";
 var price = "price=";
 var categories = "categories=";
 var city = "location=";
+var limit = "limit=5";
 var userLocationInput = "";
 var userResterauntInput = "";
 
@@ -13,26 +15,25 @@ var restaurantInput = document.getElementById("restaurant-search");
 var locationInput = document.getElementById("location-search");
 var searchBtn = document.getElementById("search-icon");
 var displayResultsPage = document.getElementById("results-page")
-var steakImg = document.getElementById("steak");
-var textImg = document.getElementById("green-speech-bubble");
-var starsImg = document.getElementById("red-stars");
+var homepageImg = document.getElementById("homepage-img");
+
 
 function getData(event){
     event.preventDefault();
     userLocationInput = locationInput.value;
     userResterauntInput = restaurantInput.value;
-//hides all the home page images when user clicks the search button
-    steakImg.setAttribute("class", "hide");
-    textImg.setAttribute("class", "hide");
-    starsImg.setAttribute("class", "hide");
-//clears our results and presents new results
+    //hides all the home page images when user clicks the search button
+    homepageImg.setAttribute("class", "hide");
+
+    //clears our results and presents new results
     displayResultsPage.textContent = "";
 
-//if you get CORS error include the cors URL infront of your API URL
-    var requestData = `${corsUrl}${yelpUrl}${city}${userLocationInput}&${categories}${userResterauntInput}`
+    //if you get CORS error include the cors URL infront of your API URL
+    var requestData = `${corsUrl}${yelpUrl}${city}${userLocationInput}&${categories}${userResterauntInput}&${limit}`
+    console.log(requestData);
 
-fetch(requestData, {
-    //must include ethis header with authorization to get access to yelp API 
+    fetch(requestData, {
+    //must include this header with authorization to get access to yelp API 
     headers:{
         "Authorization": yelpApiKey
     }
@@ -42,9 +43,10 @@ fetch(requestData, {
     })
     .then(function(data){
         console.log(data)
-
+        var resIdTracker = 0;
         for (var i = 0; i < data.businesses.length; i++) {
         //displays restraurant names on screen
+        resIdTracker = i;
         var restaurantNameResults = document.createElement("h1");
         restaurantNameResults.textContent = data.businesses[i].name;
         //displays restraurant $ price on screen
@@ -53,15 +55,58 @@ fetch(requestData, {
         //displays restraurant ratings on screen
         var restaurantRatingResults = document.createElement("p");
         restaurantRatingResults.textContent = data.businesses[i].rating;
+        //displays restraurant image on screen
+        var restaurantImgResults = document.createElement("div");
+        restaurantImgResults.textContent = data.businesses[i].image_url;
+        //giving restaurant ratings an ID of restaurant-1, restaurant-2, etc
+        restaurantRatingResults.setAttribute("id", `restaurant-${i}`)
 
+
+        //append the name price image and ratings to the results page
         displayResultsPage.append(restaurantNameResults);
+        displayResultsPage.append(restaurantImgResults);
         displayResultsPage.append(restaurantPriceResults);
         displayResultsPage.append(restaurantRatingResults);
 
-   
+    
+        var businessesId = data.businesses[i].id;
+        var reviewsUrl = "https://api.yelp.com/v3/businesses/"+ businessesId +"/reviews"
+        var requestReviewUrl =  `${corsUrl}${reviewsUrl}`
+        console.log(requestReviewUrl)
+
+
+        //fetching for yelp api reviews
+        fetch(requestReviewUrl,{
+            headers:{
+                "Authorization": yelpApiKey
+        }
+        })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+                for (var j = 0; j < 3; j++) {
+                var reviewsText = document.createElement("p");
+                reviewsText.textContent = data.reviews[j].text;
+                for(let k = 0; k < 5; k++) {
+                    console.log(data.reviews[j].text);
+                    var restaurantId = document.getElementById(`restaurant-${k}`)
+                    // restaurantId.append(reviewsText);
+                    if(restaurantId.children.length < 3) {
+                    restaurantId.append(reviewsText);
+                    }
+                }
+        }
+        });
+ 
     }
     });
+    
 }
+
+
+
 //gets data once user CLICKS the SEARCH BTN
 searchBtn.addEventListener("click", getData);
 
