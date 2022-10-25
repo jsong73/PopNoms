1d4c160dc00d6c098e6e325b7539c47a858180bc
jsong73 <jessicajinsong@gmail.com>
2022-10-24 22:39:00 -0400

    fixing my html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,8 +18,10 @@ var displayResultsPage = document.getElementById("results-page")
 var homepageImg = document.getElementById("homepage-img");
 
 
+
 function getData(event){
     event.preventDefault();
+    //makes sure user input is displayed with no case sensitivity
     userLocationInput = locationInput.value.toLowerCase();
     userResterauntInput = restaurantInput.value.toLowerCase();
     //hides all the home page images when user clicks the search button
@@ -57,18 +59,18 @@ function getData(event){
         //converts image URL from obj to actual image
         var restaurantImgResults = document.createElement("img");
         restaurantImgResults.src = data.businesses[i].image_url;
-        //giving restaurant ratings an ID of restaurant-1, restaurant-2, etc
+        //matching resteraunt reviews with resteraunt alias (since reviews come out of order)
         const reviewResults = document.createElement("ul");
         reviewResults.setAttribute ("id", data.businesses[i].alias)
         reviewResults.setAttribute("data", data.businesses[1].alias);
-
+        
         //append the name price image and ratings to the results page
         displayResultsPage.append(restaurantNameResults);
         displayResultsPage.append(restaurantPriceResults);
         displayResultsPage.append(restaurantRatingResults);
         restaurantRatingResults.appendChild(restaurantImgResults);
         restaurantRatingResults.append(reviewResults);
-
+         
         var businessesId = data.businesses[i].id;
         var reviewsUrl = "https://api.yelp.com/v3/businesses/"+ businessesId +"/reviews"
         var requestReviewUrl =  `${corsUrl}${reviewsUrl}`
@@ -88,6 +90,7 @@ function getData(event){
             for(let i=0; i < data.reviews.length; i++){
                 let restaurantUrl = data.reviews[i].url;
                 console.log(restaurantUrl);
+                //parses through yelp api alias and matches with the reviews(since reviews come out of order)
                 let parsedUrlRestraunt = restaurantUrl.split("/")[4].split("?")[0];
                 console.log(parsedUrlRestraunt)
                 const reviewsText = document.createElement("li");
@@ -101,6 +104,3 @@ function getData(event){
 
 //gets data once user CLICKS the SEARCH BTN
 searchBtn.addEventListener("click", getData);
-
-// var requestData = `${corsUrl}${yelpUrl}${city}${userInput}&${categories}&${price}&${ratings}`
-// console.log(userInput)
